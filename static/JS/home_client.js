231bed85af01ea11f94109b5ced4faf8d2676615
alexx-asm <a.alaman01@gmail.com>
2024-06-27 17:24:05 +0200

    home_client completo con menú completamente responsivo

--- static/JS/home_client.js
+++ static/JS/home_client.js
@@ -1,23 +1,37 @@
-// Añade un evento para cerrar el dropdown al hacer clic fuera de él
-document.addEventListener('click', function(event) {
-    const dropdownMenus = document.querySelectorAll('.dropdown-menu');
-    dropdownMenus.forEach(menu => {
-        if (!menu.contains(event.target)) {
-            menu.style.display = 'none';
+document.addEventListener("DOMContentLoaded", function() {
+    const navbarToggler = document.getElementById("navbarToggler");
+    const navbarMenu = document.getElementById("navbarMenu");
+
+    // Añade un evento que alterna la visibilidad del menú (navbar-menu) cuando se hace clic en el botón de hamburguesa
+    // (navbar-toggler)
+    navbarToggler.addEventListener("click", function() {
+        if (navbarMenu.style.display === "flex") {
+            navbarMenu.style.display = "none";
+        } else {
+            navbarMenu.style.display = "flex";
         }
     });
-});
 
-// Añade un evento para que el dropdown se mantenga abierto cuando se está sobre él
-const dropdownTogglers = document.querySelectorAll('.nav-item.dropdown');
-dropdownTogglers.forEach(toggler => {
-    toggler.addEventListener('mouseover', function() {
-        const dropdownMenu = toggler.querySelector('.dropdown-menu');
-        dropdownMenu.style.display = 'block';
+    // Añade un evento para que el dropdown se mantenga abierto cuando se está sobre él
+    const dropdownTogglers = document.querySelectorAll('.nav-item.dropdown');
+    dropdownTogglers.forEach(toggler => {
+        toggler.addEventListener('mouseover', function() {
+            const dropdownMenu = toggler.querySelector('.dropdown-menu');
+            dropdownMenu.style.display = 'block';
+        });
+
+        toggler.addEventListener('mouseout', function() {
+            const dropdownMenu = toggler.querySelector('.dropdown-menu');
+            dropdownMenu.style.display = 'none';
+        });
     });
 
-    toggler.addEventListener('mouseout', function() {
-        const dropdownMenu = toggler.querySelector('.dropdown-menu');
-        dropdownMenu.style.display = 'none';
+    // Añade un evento para mostrar u ocultar el menú según cambie el tamaño de la pantalla del navegador
+    window.addEventListener('resize', function(){
+        if(window.innerWidth >= 769){
+            navbarMenu.style.display = "flex";
+        } else{
+            navbarMenu.style.display = "none";
+        }
     });
 });
