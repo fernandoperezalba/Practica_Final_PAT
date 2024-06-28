5fd909d4abace12d92363b0bd213ad4073ec5ddf
FPA2000 <78817730+fpa2000@users.noreply.github.com>
2024-06-28 19:55:52 +0200

    Agregar página de usuarios y cambios menores

--- static/JS/home_admin.js
+++ static/JS/home_admin.js
@@ -1,12 +1,12 @@
 // Añade un evento para cerrar el dropdown al hacer clic fuera de él
-document.addEventListener('click', function(event) {
-    const dropdownMenus = document.querySelectorAll('.dropdown-menu');
-    dropdownMenus.forEach(menu => {
-        if (!menu.contains(event.target)) {
-            menu.style.display = 'none';
-        }
-    });
-});
+//document.addEventListener('click', function(event) {
+//    const dropdownMenus = document.querySelectorAll('.dropdown-menu');
+//    dropdownMenus.forEach(menu => {
+//        if (!menu.contains(event.target)) {
+//            menu.style.display = 'none';
+//        }
+//    });
+//});
 
 // Añade un evento para que el dropdown se mantenga abierto cuando se está sobre él
 const dropdownTogglers = document.querySelectorAll('.nav-item.dropdown');
